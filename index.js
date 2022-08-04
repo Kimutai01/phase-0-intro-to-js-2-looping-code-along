e6e67e40a4af56dc29b7cc9b1daf18b2504ee964
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-04 12:06:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arr, mess) {
+  let welcome = [];
+  for (let i = 0; i < arr.length; i++) {
+    welcome.push(`Thank you, ${arr[i]}, for the wonderful ${mess} gift!`);
+  }
+  return welcome;
+}
+
+function countDown(n) {
+  for (let i = n; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+module.exports = { writeCards, countDown };
